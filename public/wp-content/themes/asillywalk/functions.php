113cdfe84af52d1f030fed95f156ebad2241377e
AndreasMaros <andi@asillywalk.de>
2022-11-22 01:06:29 +0100

    [feat] Basic consent-tools backend with carbon-fields build step

--- public/wp-content/themes/asillywalk/functions.php
+++ public/wp-content/themes/asillywalk/functions.php
@@ -5,6 +5,10 @@ use Sillynet\Adretto\Theme;
 
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 
+define(
+    'Carbon_Fields\DIR',
+    get_theme_file_path('vendor/carbon-fields')
+);
 Theme::make(get_theme_file_path('.config.yaml'));
 BlockRegistrar::getInstance()
     ->setScriptPath('/js/editor.js')
